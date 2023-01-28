2c00b907bdf1bf045478b97f47aae6e058d6c233
gbayiola <olowogbafame@gmail.com>
2023-01-28 21:38:58 +0100

    olowogbafame@gmail.com

---
+++ trails.js
@@ -0,0 +1,17 @@
+// Fred Solution
+
+// Abraham Solution
+
+function extraLongFactorials(n) {
+    // Write your code here
+   let memoization = [BigInt(0), BigInt(1)];
+
+    const factorial = num => (typeof memoization[num] !== 'number')
+        ? ((num - BigInt(1)) > 0
+            ? (num * factorial(num - BigInt(1)))
+            : BigInt(1)
+        )
+        : memoization[num]
+
+    console.log(String(factorial(BigInt(n))));
+}
